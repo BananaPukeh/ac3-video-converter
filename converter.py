7f0d745e62a2f8bc0c320347b8aa2f085cc67156
Rutger Nijhuis <rutger.nijhuis@gmail.com>
2020-09-21 17:07:23 +0200

    Fixed multi audio track conversion and subtile passthru

--- converter.py
+++ converter.py
@@ -3,14 +3,17 @@ from pathlib import Path
 import ffmpeg
 import subprocess
 import shutil
+import requests
+import time
 
 # Paths splitted by semicolon
 libraries = os.getenv("libraries")
 validExtensions = [".mkv", ".mp4", ".m4v"]
 desiredCodecs = ["ac3", "eac3", "truehd"]
+interval = int(os.getenv('interval', "3600"))
 
 # Usefull for testing purposes
-replaceOriginal = False
+replaceOriginal = os.getenv("replace_original", "true") == "true"
 
 
 def scanLibraries():
@@ -37,7 +40,7 @@ def checkFile(path):
         probeRes = ffmpeg.probe(path)
         for stream in probeRes["streams"]:
             if stream["codec_type"] == "audio":
-                if stream["channels"] > 2:
+                if stream["channels"] > 2 or True:  # Disabled for now
                     # print("More than two channels found!")
                     codecName = stream["codec_name"]
                     if not codecName in desiredCodecs:
@@ -50,6 +53,8 @@ def checkFile(path):
                         else:
                             print("Error, don't know how to re-encode %s" %
                                   codecName)
+                            notify(
+                                "❌ Error don't know how to re-encode *{}* for:```{}```".format(codecName, path))
                         reencode(path, newCodec, codecName)
                         # Break so this file wont trigger twice if multiple audio tracks
                         break
@@ -64,6 +69,10 @@ def reencode(path, newCodec, oldCodec):
     fileName = Path(path).stem
     outputPath = os.path.join(folder, "converted.mkv")
 
+    notify("⚡ Start re-encoding *{}* to *{}*```{}```".format(oldCodec, newCodec, fileName))
+
+    startTime = time.time()
+    # Go with an direct subprocess because the python-ffmpeg api is kindoff vague
     command = [
         FFMPEG_PATH,
         "-y",  # Overwrite if exists
@@ -73,21 +82,46 @@ def reencode(path, newCodec, oldCodec):
         "copy",
         "-c:a",  # Convert only audio
         newCodec,
+        "-map",
+        "0",  # Map all audio streams
         outputPath,
     ]
 
     subprocess.call(command)
 
-    # Replace original file
-    if replaceOriginal:
-        shutil.move(outputPath, path)
+    if not os.path.exists(outputPath):
+        notify("❌ Failed to convert!")
     else:
-        newPath = os.path.join(
-            folder, fileName + " - {}.mkv".format(newCodec.upper()))
-        shutil.move(outputPath, newPath)
+        # Replace original file
+        if replaceOriginal:
+            shutil.move(outputPath, path)
+        else:
+            newPath = os.path.join(
+                folder, fileName + " - {}.mkv".format(newCodec.upper()))
+            shutil.move(outputPath, newPath)
+
+        duration = int(time.time() - startTime)
+
+        notify("✅ Done re-encoding *{}* to *{}* in {} sec```{}```".format(oldCodec,
+                                                                          newCodec, duration, fileName))
 
 
-print("Done running")
+def notify(message):
+    bot_token = os.getenv("telegram_token", "")
+    bot_chatID = os.getenv("telegram_chat_id", "")
+
+    if not bot_token == "" and not bot_chatID == "":
+        send_text = 'https://api.telegram.org/bot' + bot_token + \
+            '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message
+
+        _ = requests.get(send_text)
+
 
 # Start
-scanLibraries()
+print("Start monitoring library for unwanted audio codecs")
+print("Interval time {} seconds".format(interval))
+
+notify("🆗 Start monitoring library for unwanted audio codec.\nRunning at: `{} seconds`".format(interval))
+while True:
+    scanLibraries()
+    time.sleep(interval)
